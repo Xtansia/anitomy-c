dbfef1aa37bf2da67213606438336ad674608946
Thomas Farr <xtansia@xtansia.com>
2018-06-09 22:51:23 +1200

    Initial CMake project

---
+++ anitomy_c.h
@@ -0,0 +1,22 @@
+/*
+** Copyright (c) 2018, Thomas Farr
+**
+** This Source Code Form is subject to the terms of the Mozilla Public
+** License, v. 2.0. If a copy of the MPL was not distributed with this
+** file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+
+#ifndef ANITOMY_C_H
+#define ANITOMY_C_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
